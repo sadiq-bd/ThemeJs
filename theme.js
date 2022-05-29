abbce001cfc67ef3de2106078fa95ebdcc6e3e17
Sadiq <sadiq0698@gmail.com>
2022-05-29 15:40:56 +0600

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,7 +1,7 @@
 /**
  * Theme.js
  * @author      Sadiq Ahmed <sadiq.com.bd@gmail.com>
- * @version     1.0.0
+ * @version     1.0.2
  * @description Theme JS
  * @package     Theme
  * @category    Theme
@@ -11,44 +11,72 @@
 "use strict";
 
 function Theme(mode) {
-    if (mode === 'light' || mode === 0) {
-        mode = 0;
-        this.textColor = '#333';
-        this.backgroundColor = '#f5f5f5';
-        this.borderColor = '#ddd';
-    }
-    if (mode === 'dark' || mode === 1) {
-        mode = 1;
-        this.textColor = '#f5f5f5';
-        this.backgroundColor = '#333';
-        this.borderColor = '#444';
-    }
-    if (mode !== 0 && mode !== 1) throw new Error('Invalid Theme Mode');
-    this.mode = mode;
+    this.mode = this.getMode(mode);
+    this.setDefault();
     this.css = this.createCssElement();
 }
 
-Theme.prototype.setTextColor = function (color) {
-    this.textColor = color;
+Theme.prototype.setTextColor = function (mode, color) {
+    this.textColor[this.getMode(mode)] = color;
 };
 
-Theme.prototype.setBackgroundColor = function (color) {
-    this.backgroundColor = color;
+Theme.prototype.setBackgroundColor = function (mode, color) {
+    this.backgroundColor[this.getMode(mode)] = color;
 };
 
-Theme.prototype.setBorderColor = function (color) {
-    this.borderColor = color;
+Theme.prototype.setBorderColor = function (mode, color) {
+    this.borderColor[this.getMode(mode)] = color;
 };
 
-Theme.prototype.genarate = function () {
+Theme.prototype.getMode = function (mode) {
+    if (mode === 'light' || mode === 0) mode = 0;
+    else if (mode === 'dark' || mode === 1) mode = 1;
+    else throw new Error('Invalid Theme Mode');
+    return mode;
+}
+
+Theme.prototype.setDefault = function () {
+    this.textColor = [
+        '#333',
+        '#f5f5f5'
+    ]
+    this.backgroundColor = [
+        '#f5f5f5',
+        '#333'
+    ]
+    this.borderColor = [
+        '#444',
+        '#ddd'
+    ]
+}
+
+Theme.prototype.init = function () {
+    this.handleCss();
+}
+
+Theme.prototype.switch = function (mode) {
+    if (mode === undefined) {
+        if (this.mode === 0) this.mode = 1;
+        else if (this.mode === 1) this.mode = 0;
+    } else {
+        this.mode = this.getMode(mode);
+    }
+    this.removeCssElement();
+    this.css = this.createCssElement();
     this.handleCss();
 }
 
 Theme.prototype.createCssElement = function () {
     var css = document.createElement('style');
+    css.id = 'themejs-css';
     css.type = 'text/css';
     document.head.appendChild(css);
     return css;
+}
+
+Theme.prototype.removeCssElement = function () {
+    var css = document.getElementById('themejs-css');
+    if (css) css.remove();
 }
 
 Theme.prototype.appendCss = function (css) {
@@ -63,7 +91,18 @@ Theme.prototype.applyElement = function (element) {
     }
 }
 
-Theme.prototype.handleCss = function () {
+Theme.prototype.applyElements = function (elements) {
+    for (let i = 0; i < elements.length; i++) {
+        this.applyElement(elements[i]);
+    }   
+}
+
+Theme.prototype.handleCss = function (mode) {
+    if (mode !== undefined) {
+        mode = this.getMode(mode);
+    } else {
+        mode = this.mode;
+    }
     this.appendCss(`
         .themejs,
         .themejs *,
@@ -73,9 +112,9 @@ Theme.prototype.handleCss = function () {
         .themejs *:focus,
         .themejs *:active,
         .themejs ::placeholder {
-            background-color: ${this.backgroundColor};
-            color: ${this.textColor};
-            border-color: ${this.borderColor};
+            background-color: ${this.backgroundColor[mode]};
+            color: ${this.textColor[mode]};
+            border-color: ${this.borderColor[mode]};
         }
     `);
 }
